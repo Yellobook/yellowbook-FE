130c673a4f729d0534d3875b96c09e6bbe0fe44f
Danjjinbibbang <hyjmex@gmail.com>
2024-07-18 17:03:50 +0900

    [FE-11]: mobile 캘린다 주문글-App.jsx 수정

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
-import OrderContainer from "./mobile/calendar/order";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <OrderContainer />
+      <App />
     </RecoilRoot>
   </React.StrictMode>
 );
