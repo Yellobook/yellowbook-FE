a0444664033fe7af2bf3880d775316090f741b82
yiseoffline <lsy0476@hufs.ac.kr>
2024-08-02 20:05:21 +0900

    [FE-39]:desktop: 재고현황관리 api 연결

--- src/desktop/manage-inventory/magage-inventory.jsx
+++ src/desktop/manage-inventory/magage-inventory.jsx
@@ -1,41 +1,78 @@
 import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import { format } from "date-fns";
 import ko from "date-fns/locale/ko";
 
-const getTotalInventory = async ({ page, size }) => {
-  try {
-    const response = await axios.get(
-      `https://api.yellobook.site/api/v1/inventories?page=${page}&size=${size}`,
-      {}
-    );
-    return response.data.data.inventories;
-  } catch (error) {
-    console.error("전체 재고 현황 조회 중 오류 발생", error);
-  }
+const sampleData = {
+  isSuccess: true,
+  message: "요청이 성공적으로 처리되었습니다.",
+  data: {
+    page: 0,
+    size: 5,
+    inventories: [
+      {
+        inventoryId: 1,
+        title: "2024년 03월 25일 재고현황",
+        createdAt: "2024-03-25",
+        updatedAt: "2024-03-26",
+        view: 20,
+      },
+      {
+        inventoryId: 2,
+        title: "2024년 04월 29일 재고현황",
+        createdAt: "2024-04-29",
+        updatedAt: "2024-04-30",
+        view: 20,
+      },
+      {
+        inventoryId: 3,
+        title: "2024년 04월 15일 재고현황",
+        createdAt: "2024-04-15",
+        updatedAt: "2024-04-16",
+        view: 20,
+      },
+      {
+        inventoryId: 4,
+        title: "2024년 04월 29일 재고현황",
+        createdAt: "2024-04-29",
+        updatedAt: "2024-04-30",
+        view: 20,
+      },
+      {
+        inventoryId: 5,
+        title: "2024년 05월 09일 재고현황",
+        createdAt: "2024-05-09",
+        updatedAt: "2024-05-10",
+        view: 20,
+      },
+    ],
+  },
 };
 
 const DesktopManageInventory = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [inventories, setInventories] = useState([]);
+  const [selectedInventory, setSelectedInventory] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchInventories = async () => {
-      const inventories = await getTotalInventory({ page: 1, size: 5 });
+    const fetchInventories = () => {
+      const inventories = sampleData.data.inventories;
       setInventories(inventories);
     };
 
     fetchInventories();
   }, []);
-  const openModal = () => {
+
+  const openModal = (inventory) => {
+    setSelectedInventory(inventory);
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
+    setSelectedInventory(null);
   };
 
   return (
@@ -64,31 +101,42 @@ const DesktopManageInventory = () => {
       >
         옐로우북 이용가이드 바로가기
       </div>
-      <div
-        onClick={openModal}
-        className="cursor-pointer inline-block bg-yellow mt-10"
-      >
-        <span>api 연결 전 모달 버튼 예시</span>
-      </div>
       <div className="mt-10">
-        {inventories?.map((inventory) => (
-          <div key={inventory.inventoryId} className="border-b py-4">
-            <h3 className="text-lg font-bold">{inventory.title}</h3>
-            <p className="text-xs text-gray-500">조회수: {inventory.view}</p>
-            <p className="text-xs text-gray-500">
-              작성일자:{" "}
-              {format(new Date(inventory.createdAt), "yyyy년 MM월 dd일", {
-                locale: ko,
-              })}
-            </p>
-            <p className="text-xs text-gray-500">
-              마지막 업데이트 일시:{" "}
-              {format(new Date(inventory.updatedAt), "yyyy년 MM월 dd일", {
-                locale: ko,
-              })}
-            </p>
-          </div>
-        ))}
+        {inventories.length > 0 ? (
+          inventories
+            .slice()
+            .reverse()
+            .map((inventory) => (
+              <div
+                key={inventory.inventoryId}
+                className="border-t py-4 cursor-pointer"
+                onClick={() => openModal(inventory)}
+              >
+                <div className="flex justify-between">
+                  <h3 className="text-lg font-bold">{inventory.title}</h3>
+                  <p className="text-xs text-customGray1">
+                    작성일자:{" "}
+                    {format(new Date(inventory.createdAt), "yyyy년 MM월 dd일", {
+                      locale: ko,
+                    })}
+                  </p>
+                </div>
+                <div className="flex justify-between mt-5">
+                  <p className="text-xs text-customGray1">
+                    조회수: {inventory.view}
+                  </p>
+                  <p className="text-xs text-customGray1">
+                    마지막 업데이트 일시:{" "}
+                    {format(new Date(inventory.updatedAt), "yyyy년 MM월 dd일", {
+                      locale: ko,
+                    })}
+                  </p>
+                </div>
+              </div>
+            ))
+        ) : (
+          <p>재고 현황을 불러오는 중입니다...</p>
+        )}
       </div>
       <Modal
         isOpen={isModalOpen}
@@ -103,15 +151,25 @@ const DesktopManageInventory = () => {
         >
           x
         </div>
-        <h2 style={{ color: "#FFAB08" }}>YYYY MM월 DD일 재고현황</h2>
-        <div className="p-4">표</div>
-        <button
-          onClick={() => navigate("/manage-inventory/edit")}
-          className="mb-5 p-2 rounded-md text-xs"
-          style={{ backgroundColor: "#FFDE33" }}
-        >
-          재고현황 수정하기
-        </button>
+        {selectedInventory && (
+          <>
+            <h2 style={{ color: "#FFAB08" }}>
+              {format(
+                new Date(selectedInventory.createdAt),
+                "yyyy년 MM월 dd일"
+              )}{" "}
+              재고현황
+            </h2>
+            <div className="p-4">표</div>
+            <button
+              onClick={() => navigate("/manage-inventory/edit")}
+              className="mb-5 p-2 rounded-md text-xs"
+              style={{ backgroundColor: "#FFDE33" }}
+            >
+              재고현황 수정하기
+            </button>
+          </>
+        )}
       </Modal>
     </div>
   );
