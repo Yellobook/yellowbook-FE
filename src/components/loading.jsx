cf7ea061c2377ec80e947068b744d865fb02f098
yunchan312 <phenomenal312@hufs.ac.kr>
2024-08-10 11:29:25 +0900

    [FE-42] cookie logic

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -15,9 +15,7 @@ export default function Loading() {
       err = e;
       console.log(e);
     } finally {
-      if (!err && cookie) {
-        navigate("");
-      }
+      navigate("");
     }
   }, [localStorage, cookie]);
   return (
