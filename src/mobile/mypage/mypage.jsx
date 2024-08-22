1a001665c7651a02332f1194d218aeb4447e83d2
yunchan312 <phenomenal312@hufs.ac.kr>
2024-08-22 22:22:36 +0900

    [FE-72] mobile profile fix

--- src/mobile/mypage/mypage.jsx
+++ src/mobile/mypage/mypage.jsx
@@ -1,15 +1,47 @@
+import { useEffect, useState } from "react";
+import { getProfile } from "../../util/ProfileUtils";
+
 export default function MobileMyPage() {
+  const [profile, setProfile] = useState({});
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const data = await getProfile();
+        setProfile(data);
+        console.log(data); // 프로필 데이터를 콘솔에 출력합니다.
+      } catch (error) {
+        console.error("프로필을 불러오는 중 오류 발생:", error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
   return (
     <div className="flex flex-col gap-10">
       <div className="flex py-5 items-center gap-5">
-        <div className="size-[98px] rounded-full bg-orange"></div>
+        <div
+          className="size-[98px] rounded-full bg-orange bg-center bg-cover"
+          style={{
+            backgroundImage: `url("${profile.profileImage}")`,
+          }}
+        ></div>
         <div>
-          <div className="text-orange text-[23px]">username</div>
-          <div className="flex items-center justify-between text-[13px] opacity-50">
-            <span>role</span>
-            <span>|</span>
-            <span>team name</span>
-          </div>
+          <div className="text-orange text-[23px]">{profile.nickname}</div>
+          <>
+            {profile.teams
+              ? profile.teams.map((t, ind) => (
+                  <div
+                    key={ind}
+                    className="flex items-center justify-between text-[13px] opacity-50 gap-2"
+                  >
+                    <span>{t.teamName}</span>
+                    <span>|</span>
+                    <span>{t.role}</span>
+                  </div>
+                ))
+              : null}
+          </>
         </div>
       </div>
 
